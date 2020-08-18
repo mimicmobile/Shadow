0a208de553f9f0d2b482af357d570e5fd84c4044
Jeff Corcoran <jcorcoran+github@gmail.com>
2020-08-18 16:26:28 -0400

    Add color param

--- lib/shadow.dart
+++ lib/shadow.dart
@@ -2,43 +2,47 @@ library shadow;
 
 import 'package:flutter/material.dart';
 
-
-class Shadow extends StatelessWidget{
-
+class Shadow extends StatelessWidget {
   final double opacity;
   final double scale;
   final Widget child;
   final Offset offset;
-
+  final Color color;
 
   Shadow({
     this.opacity,
+    this.color,
     @required this.child,
     this.scale,
     this.offset,
-  }): assert(child != null);
+  }) : assert(child != null);
 
   @override
   Widget build(BuildContext context) {
-
     return Stack(
-      children: <Widget>[ 
-
-           Transform.translate(
-
-             offset: offset ?? Offset(5,5),
-             child: 
-             
-             Transform.scale(
-             scale: scale ?? 1,  
-             child:Opacity(
-               opacity: opacity ?? 0.5,
-               child: child),
-           ),),
-           child,
-          ],
-        );
+      children: <Widget>[
+        Transform.translate(
+          offset: offset ?? Offset(5, 5),
+          child: Transform.scale(
+            scale: scale ?? 1,
+            child: Opacity(opacity: opacity ?? 0.5, child: changeColor(child)),
+          ),
+        ),
+        child,
+      ],
+    );
   }
 
+  Widget changeColor(Widget child) {
+    if (color != null) {
+      return ColorFiltered(
+        colorFilter: ColorFilter.mode(
+          color,
+          BlendMode.saturation,
+        ),
+        child: child,
+      );
+    }
+    return child;
+  }
 }
-
